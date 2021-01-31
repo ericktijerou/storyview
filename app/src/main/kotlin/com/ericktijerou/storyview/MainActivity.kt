40ffdc2536283b3073721de2f563f2887b1390e3
Erick Tijero <ericktijerou@gmail.com>
2021-01-31 18:24:50 -0500

    Set adapter to viewpager

--- app/src/main/kotlin/com/ericktijerou/storyview/MainActivity.kt
+++ app/src/main/kotlin/com/ericktijerou/storyview/MainActivity.kt
@@ -2,14 +2,23 @@ package com.ericktijerou.storyview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ericktijerou.storyview.StoryHelper.generateDataSet
 import com.ericktijerou.storyview.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), StoryOnClick {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        initView()
+    }
+
+    private fun initView() {
+        binding.run {
+            viewPager.adapter = StoryAdapter(this@MainActivity, generateDataSet())
+            viewPager.setPageTransformer(CubeOutTransformer())
+        }
     }
 }
